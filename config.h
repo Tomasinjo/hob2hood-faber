d3e608086e2f299bd14c4f9d3dcd447363228263
Tomasinjo <43334457+Tomasinjo@users.noreply.github.com>
2021-03-14 23:02:21 +0100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,9 @@
+// An IR detector/demodulator is connected to GPIO pin 14(D5 on a NodeMCU
+// board).
+#define RECV_PIN 14
+
+// An IR LED is connected to GPIO pin 12 (D6 on a NodeMCU
+// board).
+#define IR_LED 12
+
+
